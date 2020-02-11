a6824733ba112d6ea99d62a361ad85652ec398ae
jhSeo <jh.seo@battleent.com>
2020-02-11 12:04:44 +0900

    Fix to get the viewcontroller directly

--- Sources/StoryboardFramework/SDKViewController.swift
+++ Sources/StoryboardFramework/SDKViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 public final class SDKViewController: UIViewController {
 
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override public func loadView() {
+        guard self.nibName == nil else { return super.loadView() }
+
+        let storyboard = UIStoryboard(name: "SDKView", bundle: Bundle(for: SDKViewController.self))
+        guard let storyboardInstance = storyboard.instantiateViewController(withIdentifier: "SDKViewController") as? SDKViewController else { fatalError() }
+
+        let storyboardView = storyboardInstance.view
+        storyboardInstance.view.removeFromSuperview()
+        storyboardInstance.view = nil
+
+        self.navigationItem.title = storyboardInstance.navigationItem.title
+        self.view = storyboardView
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
     @IBAction func okBtnDidTap(_ sender: Any) {
         var isModal: Bool {
             if (self.presentingViewController != nil) {
